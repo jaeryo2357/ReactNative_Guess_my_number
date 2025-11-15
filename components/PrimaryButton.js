2d48d1d90400b6d31d9a781104043119c2995824
jaeryo2357 <jaeryo2357@naver.com>
2025-11-15 18:22:11 +0900

    시작화면 버튼 스타일링 하기

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,46 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
+
+    function pressHandler() {
+        console.log("pressed")
+    }
+
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.buttonOutContainer}>
+            <Pressable 
+             android_ripple={{ color: '#91305cff' }}
+             style={({pressed}) => pressed ? [
+                styles.buttonInnerContainer, styles.pressed
+             ] : styles.buttonInnerContainer}
+             onPress={pressHandler}
+             >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
         </View>
     );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOutContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden',
+    },
+    buttonInnerContainer: {
+        backgroundColor: "#720637",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        borderRadius: 28,
+        elevation: 2,
+    },
+    buttonText: {
+        color: "white",
+        textAlign: "center",
+    },
+    pressed: {
+        opacity: 0.75
+    }
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,9 +4,19 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput/>
-            <PrimaryButton>reset</PrimaryButton>
-            <PrimaryButton>confirm</PrimaryButton>
+            <TextInput style={styles.numberInput}
+                maxLength={2} keyboardType="number-pad"
+                autoCorrect={false}
+                autoCapitalize="none"
+            />
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
         </View>
     );
 }
@@ -19,11 +29,28 @@ const styles = StyleSheet.create({
         marginHorizontal: 24,
         padding: 16,
         borderRadius: 8,
-        backgroundColor: '#720637',
+        backgroundColor: '#460522ff',
         elevation: 4,
         shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2},
+        shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
-        shadowOpacity: 0.25
+        shadowOpacity: 0.25,
+        alignItems: 'center'
+    },
+    numberInput: {
+        width: 100,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        color: '#ddb52f',
+        borderBottomWidth: 2,
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1
     }
 });
